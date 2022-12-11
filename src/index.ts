cd1b1f4ed4cfa35dd959f8661537b35d8fa3e202
Glazkov Pavel <glazprom@mail.ru>
2022-12-11 17:02:34 +0300

    feat: add router

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import * as imgPhotoVideo from "../static/images/photo-video.svg";
 import * as imgFile from "../static/images/file.svg";
 import * as imgLocation from "../static/images/location.svg";
 import {IProfileState} from './components/profile/interfaces/profile-state.interface';
-import {render, renderPage} from './utils/render';
+import Router from './utils/router';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const chat = new Chat({
@@ -46,48 +46,56 @@ document.addEventListener('DOMContentLoaded', async () => {
     avatar: 'https://sun9-north.userapi.com/sun9-88/s/v1/if1/Ut6lK2K0J5PgfQ315J18BI2BIryVYtizUK6IXM2HMwUbpF2cMbObnEzUNcncenN2cd0ZN9en.jpg?size=2160x2160&quality=96&type=album',
   }
 
-  const profile = new Profile(state, user);
-  const signIn = new SignIn();
-  const signUp = new SignUp();
-  const notFound = new NotFound();
-  const serverError = new ServerError();
+  // const profile = new Profile(state, user);
+  // const signIn = new SignIn();
+  // const signUp = new SignUp();
+  // const notFound = new NotFound();
+  // const serverError = new ServerError();
+
+  Router.use('/sign-in', SignIn)
+    .use('/sign-up', SignUp)
+    .use('not-found', NotFound)
+    .use('server-error', ServerError)
+    .use('/chat', Chat)
+    .use('/profile', Profile)
+  Router.start();
 
 
   // при загрузке страницы в тэг app подставляется содержимое страницы Логин
-  render("#root", signIn);
-
-  const path = document.location.pathname; // смотрим где мы
-
-  switch (path) {
-    case "/":
-      break;
-
-    case "/signIn":
-      renderPage(signIn);
-      break;
-
-    case "/signUp":
-      renderPage(signUp);
-      break;
-
-    case "/notFound":
-      renderPage(notFound);
-      break;
-
-    case "/serverError":
-      renderPage(serverError);
-      break;
-
-    case "/chat":
-      renderPage(chat);
-      break;
-
-    case "/profile":
-      renderPage(profile);
-      break;
-
-    default:
-      renderPage(notFound);
-  }
+  // render("#root", signIn);
+  //
+  // const path = document.location.pathname; // смотрим где мы
+  //
+  // switch (path) {
+  //   case "/":
+  //     break;
+  //
+  //   case "/signIn":
+  //     renderPage(signIn);
+  //     break;
+  //
+  //   case "/signUp":
+  //     renderPage(signUp);
+  //     break;
+  //
+  //   case "/notFound":
+  //     renderPage(notFound);
+  //     break;
+  //
+  //   case "/serverError":
+  //     renderPage(serverError);
+  //     break;
+  //
+  //   case "/chat":
+  //     renderPage(chat);
+  //     break;
+  //
+  //   case "/profile":
+  //     renderPage(profile);
+  //     break;
+  //
+  //   default:
+  //     renderPage(notFound);
+  // }
 
 })
