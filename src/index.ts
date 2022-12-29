c4a130aa7a016719e52a71511be924f7aa39a8d2
Pavel Glazkov <glazprom@mail.ru>
2022-12-29 20:25:28 +0300

    feat: work with chat

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,11 @@ import {Chat} from "./components/chat/chat";
 import Router from './utils/router';
 import {Routes} from './components/shared/enums/routes.enum';
 import {ProfilePage} from "./components/profile/profile";
+import AuthController from "./controllers/auth-controller";
 
 document.addEventListener('DOMContentLoaded', async () => {
+  let isProtectedRoute = true;
+  const path = document.location.pathname;
 
   Router.use(Routes.SIGN_IN, SignIn)
     .use(Routes.SIGN_UP, SignUp)
@@ -15,6 +18,30 @@ document.addEventListener('DOMContentLoaded', async () => {
     .use(Routes.SERVER_ERROR, ServerError)
     .use(Routes.MESSENGER, Chat)
     .use(Routes.SETTING, ProfilePage)
-  Router.start();
+
+  switch (path) {
+    case Routes.SIGN_IN:
+    case Routes.SIGN_UP:
+      isProtectedRoute = false;
+      break;
+  }
+
+
+  try {
+    await AuthController.fetchUser();
+
+    Router.start();
+
+    if (!isProtectedRoute) {
+      Router.go(Routes.MESSENGER)
+    }
+  } catch (e) {
+    Router.start();
+
+    if (isProtectedRoute) {
+      Router.go(Routes.SIGN_IN);
+    }
+  }
+
 
 })
