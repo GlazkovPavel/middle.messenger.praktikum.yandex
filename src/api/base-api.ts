0c025d30df17165b2d48b17bcd3b5678bcaa04ad
Pavel Glazkov <glazprom@mail.ru>
2023-01-08 13:18:27 +0300

    feat(sprint_4): work with unit tests

--- src/api/base-api.ts
+++ src/api/base-api.ts
@@ -1,4 +1,4 @@
-import {HTTPTransport} from '../utils/api';
+import {HTTPTransport} from '../utils/HTTPTransport';
 
 export default abstract class BaseAPI {
   protected http: HTTPTransport;
--- src/utils/block.test.ts
+++ src/utils/block.test.ts
@@ -1,14 +1,27 @@
-import {Block} from "./block";
+import type BlockType from './block'
+import sinon from "sinon";
+import proxyquire from "proxyquire";
+import {expect} from "chai";
 
-describe('Block', () => {
-  class Component extends Block<{}> {
-    render() {
-      return new DocumentFragment();
+describe.only('Block', () => {
+  const eventBusMock = {
+    on: sinon.fake(),
+    emit: sinon.fake(),
+  };
+  const { default: Block } = proxyquire('./block', {
+    './event-bus': {
+      EventBus: class {
+        emit = eventBusMock.emit;
+        on = eventBusMock.on;
+      },
+      '@noCallThru': true,
     }
-  }
+  }) as { default: typeof BlockType };
+  class ComponentMock extends Block {}
 
-  it('test', () => {
-    const instance = new Component({})
-    console.log(instance);
+  it('should fire init event on initialization',  () => {
+    new ComponentMock({});
+
+    expect(eventBusMock.emit.calledWith('init')).to.eq(true);
   });
-})
+});
