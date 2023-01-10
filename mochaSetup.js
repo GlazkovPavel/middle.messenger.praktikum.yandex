8947201166c39c361b06e91e27fb307d1884b3d2
Pavel Glazkov <glazprom@mail.ru>
2023-01-10 20:15:27 +0300

    fix(sprint_4): fix after review

--- mochaSetup.js
+++ mochaSetup.js
@@ -2,9 +2,9 @@ const { JSDOM } = require('jsdom');
 const fs = require('fs');
 const Handlebars = require('handlebars');
 
-const dom = new JSDOM(`<!DOCTYPE html><html><body><div id="app"></div></body></html>`, {
+const dom = new JSDOM('<!DOCTYPE html><html><body><div id="app"></div></body></html>', {
   url: 'http://localhost:3000',
-})
+});
 
 global.window = dom.window;
 global.document = dom.window.document;
@@ -14,8 +14,8 @@ require.extensions['.hbs'] = (module, filename) => {
   const contents = fs.readFileSync(filename, 'utf-8');
 
   module.exports = Handlebars.compile(contents);
-}
+};
 
 require.extensions['.less'] = function (module) {
   module.exports = {};
-}
+};
